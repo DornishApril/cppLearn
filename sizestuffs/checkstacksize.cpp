b6790680fad32f060a661c9536980ce08c12b54b
DornishApril <samireformed786@gmail.com>
2024-11-07 23:18:13 +0600

    using [size+inc] lets a lottttt of stack memory available, but doing the same thing with actual number makes it having less idk why pardon my english

---
+++ sizestuffs/checkstacksize.cpp
@@ -0,0 +1,33 @@
+#include <windows.h>
+#include <malloc.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <iostream>
+
+using namespace std;
+
+static uint64_t GetAvailableStackSpace() {
+    volatile uint8_t var;
+    MEMORY_BASIC_INFORMATION mbi;
+
+    auto virtualQuerySuccess = VirtualQuery((LPCVOID)&var, &mbi, sizeof(mbi));
+
+    if (!virtualQuerySuccess) {
+        return 0;
+    }
+
+    return (uint64_t)&var - (uint64_t)mbi.AllocationBase;
+}
+
+int main() {
+    
+    cout << "Available stack memory = " << GetAvailableStackSpace()<< " bytes\n\n\n";
+    
+    //524064
+    int inc=1;
+    int a[520000+inc];
+    a[520000]=10;
+    
+
+    return 0;
+}
--- sizestuffs/staticmem.cpp
+++ sizestuffs/staticmem.cpp
@@ -1,10 +1,27 @@
 int main(){
-
-    int size = 20000000;
     //int size = 20000000;
+    //1MB stack size wud give roughly 250,000 member array limit for int
     //this size gives some kind of memory error, return code=3221225725
 
-    int a[size];
+    int size1 = 20000000;
+    int size2 = 520000;
+    //          250000 is the limit
+    //  size2 = 200000 works
+    //  size2 = 250000 works
+    //  size2 = 250001 works
+    //  size2 = 300000 works
+    //  size2 = 1000000 works not
+    //  size2 = 500000 works
+    //  size2 = 900000 works not
+    //  size2 = 800000 works not
+    //  size2 = 500001 works
+    //  size2 = 550000 works not
+    //  size2 = 520000 works 530000 not
+
+
+
+  
+    int a[size2];
 
     
 
