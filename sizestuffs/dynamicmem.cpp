49405cabd2eae559b89152179cad39331e0a8f9d
DornishApril <samireformed786@gmail.com>
2024-11-07 16:44:18 +0600

    statically crashes dynamically doesnt

---
+++ sizestuffs/dynamicmem.cpp
@@ -0,0 +1,7 @@
+int main(){
+    int size = 20000000;
+
+    int *a = new int(size);
+
+    delete a;
+}
---
+++ sizestuffs/staticmem.cpp
@@ -0,0 +1,12 @@
+int main(){
+
+    int size = 20000000;
+    //int size = 20000000;
+    //this size gives some kind of memory error, return code=3221225725
+
+    int a[size];
+
+    
+
+
+}
